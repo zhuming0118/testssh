6569031e3ca07abed63fdd26666019796514ce1c
zhuming0118 <1269667479@qq.com>
2022-08-08 14:55:51 +0800

    create bixiejianfa

---
+++ src/main/java/com/atguigu/git/BiXieJianFa.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+public class BiXieJianFa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
